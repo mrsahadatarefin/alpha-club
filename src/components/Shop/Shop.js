ac8d90d43338a9ce48ec3a05bc88efd163db8fed
mrsahadatarefin <sahadatarifin806@gmail.com>
2022-09-29 23:04:44 +0600

    all done

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -18,7 +18,7 @@ const [breakTime,setBreakTime]=useState(0)
     let totalTime = 0;
     for (let i = 0; i < newCart.length; i++) {
       const product = newCart[i];
-      totalTime = totalTime + Number(product.time);
+      totalTime = totalTim + Number(product.time);
     }
     setTotal(totalTime);
     setCart(newCart);
